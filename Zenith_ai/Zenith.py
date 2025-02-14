f2670a5ef277d696aa1696b79d99a1b87a797298
mahendrabella1 <137097022+mahendrabella1@users.noreply.github.com>
2025-02-14 12:53:08 +0530

    Update Zenith.py

--- Zenith_ai/Zenith.py
+++ Zenith_ai/Zenith.py
@@ -120,7 +120,7 @@ def query_chatbot(question, use_model_only=False):
                 {"role": "system", "content": "You are an advanced AI assistant, ready to answer any query."},
                 {"role": "user", "content": question}
             ],
-            model="qwen-2.5-32b",
+            model="llama-3.3-70b-versatile"
             stream=False,
         )
         return chat_completion.choices[0].message.content
@@ -145,7 +145,7 @@ def query_chatbot(question, use_model_only=False):
             {"role": "system", "content": "You are an advanced AI assistant, ready to answer any query."},
             {"role": "user", "content": f"Relevant Information:\n\n{retrieved_text}\n\nUser's question: {question}"}
         ],
-        model="qwen-2.5-32b",
+        model="llama-3.3-70b-versatile,
         stream=False,
     )
 
